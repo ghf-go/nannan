171a4d080c1c48684f1f0ce392d6886ed0e58234
耿鸿飞 <genghongfei@yidian-inc.com>
2021-12-15 19:45:31 +0800

    提供测试用例

--- Test.go
+++ Test.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"github.com/ghf-go/nannan/app"
+	"github.com/ghf-go/nannan/glog"
+	"github.com/ghf-go/nannan/web"
 	"os"
 )
 
 func main() {
 	os.Setenv("app.web",":9081")
+	web.RegisterMiddleWare(func(e *web.EngineCtx ,f func(*web.EngineCtx)){
+		e.Header().Add("aa","bb")
+		glog.Debug("测试1")
+		f(e)
+		glog.Debug("ceshi 2")
+	})
 	app.Run()
 }
