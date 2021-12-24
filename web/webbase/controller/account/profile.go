0c7c1133d40b8e00e865f976b042593833fa320f
飞哥 <eoe2005@qq.com>
2021-12-24 22:42:48 +0800

    设置用户基本资料

--- web/webbase/controller/account/profile.go
+++ web/webbase/controller/account/profile.go
@@ -7,15 +7,19 @@ import (
 
 //设置用资料
 func SetProfileAction(engine *web.EngineCtx) error {
-	req := &reqBaseUid{}
-	engine.Verify(req)
-	if req.Uid == 0 {
-		req.Uid = engine.ForceUID()
+	engine.Req.ParseMultipartForm(999999)
+	uid := engine.ForceUID()
+	pData := map[string]interface{}{}
+	for k := range engine.Req.Form {
+		pData[k] = engine.Req.FormValue(k)
 	}
-	return engine.JsonSuccess(accountlogic.GetProfileByUid(req.Uid))
+
+	accountlogic.SetProfile(uid, pData)
+	return engine.JsonSuccess(accountlogic.GetProfileByUid(uid))
 }
 
 //获取用户资料
 func GetProfileAction(engine *web.EngineCtx) error {
-	return nil
+	uid := engine.ForceUID()
+	return engine.JsonSuccess(accountlogic.GetProfileByUid(uid))
 }
