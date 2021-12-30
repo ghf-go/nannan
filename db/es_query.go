fcf436539a9b29abed97cba1fd710212c5f54535
耿鸿飞 <genghongfei@yidian-inc.com>
2021-12-30 11:26:54 +0800

    es 增加in操作

--- db/es_query.go
+++ db/es_query.go
@@ -53,7 +53,7 @@ func (eq *esQeury) Order(key, sort string) *esQeury {
 	}
 	return eq
 }
-func (eq *esQeury) mustkv(k, key, val string) *esQeury {
+func (eq *esQeury) mustkv(k, key string, val interface{}) *esQeury {
 	eq.must = append(eq.must, map[string]map[string]interface{}{
 		k: {
 			key: val,
@@ -61,7 +61,7 @@ func (eq *esQeury) mustkv(k, key, val string) *esQeury {
 	})
 	return eq
 }
-func (eq *esQeury) mustnotkv(k, key, val string) *esQeury {
+func (eq *esQeury) mustnotkv(k string, key string, val interface{}) *esQeury {
 	eq.must_not = append(eq.must_not, map[string]map[string]interface{}{
 		k: {
 			key: val,
@@ -69,7 +69,7 @@ func (eq *esQeury) mustnotkv(k, key, val string) *esQeury {
 	})
 	return eq
 }
-func (eq *esQeury) shouldtkv(k, key, val string) *esQeury {
+func (eq *esQeury) shouldtkv(k string, key string, val interface{}) *esQeury {
 	eq.should = append(eq.should, map[string]map[string]interface{}{
 		k: {
 			key: val,
@@ -78,23 +78,26 @@ func (eq *esQeury) shouldtkv(k, key, val string) *esQeury {
 	return eq
 }
 
-func (eq *esQeury) MustMatch(key, val string) *esQeury {
+func (eq *esQeury) MustMatch(key string, val interface{}) *esQeury {
 	return eq.mustkv("match", key, val)
 }
 
-func (eq *esQeury) MustTerm(key, val string) *esQeury {
+func (eq *esQeury) MustTerm(key string, val interface{}) *esQeury {
 	return eq.mustkv("term", key, val)
 }
-func (eq *esQeury) MustWildcard(key, val string) *esQeury {
+func (eq *esQeury) MustTerms(key string, args ...interface{}) *esQeury {
+	return eq.mustkv("terms", key, args)
+}
+func (eq *esQeury) MustWildcard(key string, val interface{}) *esQeury {
 	return eq.mustkv("wildcard", key, val)
 }
-func (eq *esQeury) MustPrefix(key, val string) *esQeury {
+func (eq *esQeury) MustPrefix(key string, val interface{}) *esQeury {
 	return eq.mustkv("prefix", key, val)
 }
-func (eq *esQeury) MustText(key, val string) *esQeury {
+func (eq *esQeury) MustText(key string, val interface{}) *esQeury {
 	return eq.mustkv("text", key, val)
 }
-func (eq *esQeury) MustQuery(key, val string) *esQeury {
+func (eq *esQeury) MustQuery(key string, val interface{}) *esQeury {
 	eq.must = append(eq.must, map[string]map[string]interface{}{
 		"query_string": {
 			"default_field": key,
@@ -103,7 +106,7 @@ func (eq *esQeury) MustQuery(key, val string) *esQeury {
 	})
 	return eq
 }
-func (eq *esQeury) MustNotQuery(key, val string) *esQeury {
+func (eq *esQeury) MustNotQuery(key string, val interface{}) *esQeury {
 	eq.must_not = append(eq.must_not, map[string]map[string]interface{}{
 		"query_string": {
 			"default_field": key,
@@ -112,7 +115,7 @@ func (eq *esQeury) MustNotQuery(key, val string) *esQeury {
 	})
 	return eq
 }
-func (eq *esQeury) ShouldQuery(key, val string) *esQeury {
+func (eq *esQeury) ShouldQuery(key string, val interface{}) *esQeury {
 	eq.should = append(eq.should, map[string]map[string]interface{}{
 		"query_string": {
 			"default_field": key,
@@ -186,37 +189,37 @@ func (eq *esQeury) ShouldRange(key, opStart string, start interface{}, opEnd str
 	return eq
 }
 
-func (eq *esQeury) MustNotMatch(key, val string) *esQeury {
+func (eq *esQeury) MustNotMatch(key string, val interface{}) *esQeury {
 	return eq.mustnotkv("match", key, val)
 }
 
-func (eq *esQeury) MustNotTerm(key, val string) *esQeury {
+func (eq *esQeury) MustNotTerm(key string, val interface{}) *esQeury {
 	return eq.mustnotkv("term", key, val)
 }
-func (eq *esQeury) MustNotWildcard(key, val string) *esQeury {
+func (eq *esQeury) MustNotWildcard(key string, val interface{}) *esQeury {
 	return eq.mustnotkv("wildcard", key, val)
 }
-func (eq *esQeury) MustNotPrefix(key, val string) *esQeury {
+func (eq *esQeury) MustNotPrefix(key string, val interface{}) *esQeury {
 	return eq.mustnotkv("prefix", key, val)
 }
-func (eq *esQeury) MustNotText(key, val string) *esQeury {
+func (eq *esQeury) MustNotText(key string, val interface{}) *esQeury {
 	return eq.mustnotkv("text", key, val)
 }
 
-func (eq *esQeury) ShouldMatch(key, val string) *esQeury {
+func (eq *esQeury) ShouldMatch(key string, val interface{}) *esQeury {
 	return eq.shouldtkv("match", key, val)
 }
 
-func (eq *esQeury) ShouldTerm(key, val string) *esQeury {
+func (eq *esQeury) ShouldTerm(key string, val interface{}) *esQeury {
 	return eq.shouldtkv("term", key, val)
 }
-func (eq *esQeury) ShouldWildcard(key, val string) *esQeury {
+func (eq *esQeury) ShouldWildcard(key string, val interface{}) *esQeury {
 	return eq.shouldtkv("wildcard", key, val)
 }
-func (eq *esQeury) ShouldPrefix(key, val string) *esQeury {
+func (eq *esQeury) ShouldPrefix(key string, val interface{}) *esQeury {
 	return eq.shouldtkv("prefix", key, val)
 }
-func (eq *esQeury) ShouldText(key, val string) *esQeury {
+func (eq *esQeury) ShouldText(key string, val interface{}) *esQeury {
 	return eq.shouldtkv("text", key, val)
 }
 func (eq *esQeury) Missing(key string) *esQeury {
