7cb51ea67aff1205bfd267ad1b8ebcd16fd71eb0
耿鸿飞 <genghongfei@yidian-inc.com>
2022-01-06 20:52:39 +0800

    fx

--- drivers/confdriver/etcd.go
+++ drivers/confdriver/etcd.go
@@ -3,12 +3,11 @@ package confdriver
 import (
 	"context"
 	clientv3 "go.etcd.io/etcd/client/v3"
-	"os"
 	"strconv"
-	"strings"
 )
 
 type EtcdDriver struct {
+	path   string
 	client *clientv3.Client
 	ctx    context.Context
 	conf   clientv3.Config
@@ -34,22 +33,25 @@ func NewEtcdDriverByConf(config Conf) *EtcdDriver {
 	return &EtcdDriver{
 		client: client,
 		ctx:    context.Background(),
+		path:   config.Path,
 		conf:   ec,
 	}
 }
 func (c *EtcdDriver) Get(key string) string {
-	r, e := c.client.Get(c.ctx, key)
+	r, e := c.client.Get(c.ctx, c.path+key)
 	if e != nil {
 		return ""
 	}
 	return string(r.Kvs[0].Value)
 }
 func (c *EtcdDriver) All() map[string]string {
-	list := os.Environ()
 	ret := map[string]string{}
-	for _, v := range list {
-		i := strings.Index(v, "=")
-		ret[v[:i]] = v[i+1:]
+	r, e := c.client.Get(c.ctx, c.path)
+	if e != nil {
+
+	}
+	for _, item := range r.Kvs {
+		ret[string(item.Key)] = string(item.Value)
 	}
 	return ret
 }
@@ -78,13 +80,13 @@ func (c *EtcdDriver) GetFloat(key string) float64 {
 	return r
 }
 func (c *EtcdDriver) Del(key string) {
-	c.Set(key, "")
+	c.client.Delete(c.ctx, c.path+key)
 }
 func (c *EtcdDriver) GetConf(key string) Conf {
 	return BuildConf(c.Get(key))
 }
 func (c *EtcdDriver) Set(key, val string) {
-	c.client.Put(c.ctx, key, val)
+	c.client.Put(c.ctx, c.path+key, val)
 	c.client.Sync(c.ctx)
 }
 func (c *EtcdDriver) SetInt(key string, val int64) {
